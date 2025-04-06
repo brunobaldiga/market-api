09a0e7da0886c763987a37a7bd8dba0128bcbd04
Bruno <brunobaldiga1@gmail.com>
2025-04-06 12:11:33 -0300

    feat: Initializing project w/ configuration

---
+++ src/test/java/dev/bruno/marketapi/MarketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bruno.marketapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
